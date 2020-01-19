5383be7a818bd47dd6a13d03cb208f15ba004138
Professor322 <vlegros@21-SCHOOL.RU>
2020-01-19 21:00:17 +0300

    forgotten files to commit for previous one

--- main.c
+++ main.c
@@ -25,10 +25,10 @@ int 	main(int argc, char **argv)
 	t_champ *champ;
 
 	champ = champ_init();
-	parse_header(champ, fd);
+
+	ft_parse(fd, champ);
 	printf("name: %s\ncomment: %s\n", champ->name, champ->comment);
-	/*ft_parse(fd, champ);
 	write_exec_code_in_file(open("test.cor", O_CREAT | O_WRONLY, 0644),
-			champ->command_vec, "test.cor");*/
+			champ->command_vec, "test.cor");
 	return (0);
 }
