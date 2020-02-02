f81029fd81afbe6c1a6b13ba857073096fd1ebc3
Professor322 <vlegros@21-SCHOOL.RU>
2020-02-02 18:58:21 +0300

    adding stock asm, champions and name parser

--- main.c
+++ main.c
@@ -19,17 +19,31 @@ t_champ *champ_init()
 	champ->comment = NULL;
 	return (champ);
 }
+#define COR 4
+
+char *cor_file_name(char *argv) {
+	char *file_name_end = ft_strchr(argv, '.');
+	char *name = ft_strnew(file_name_end - argv + COR);
+	*file_name_end = '\0';
+	ft_strcpy(name, argv);
+	name[file_name_end - argv] = '.';
+	name[file_name_end - argv + 1] = 'c';
+	name[file_name_end - argv + 2] = 'o';
+	name[file_name_end - argv + 3] = 'r';
+	return 	name;
+}
 
 int 	main(int argc, char **argv)
 {
 	int fd = open(argv[1], O_RDONLY);
+	char *name = cor_file_name(argv[1]);
 	t_champ *champ;
 
-	champ = champ_init();
 
-	ft_parse(fd, champ);
-	write_exec_code_in_file(open("test.cor",  O_WRONLY, 0644),
-			champ->command_vec, "test.cor");
+	champ = champ_init();
+		ft_parse(fd, champ);
+	write_exec_code_in_file(open(name,  O_WRONLY | O_CREAT, 0644),
+			champ->command_vec, name);
 
 	printf("\n");
 	for (int i = 0; i < (int)champ->command_vec->length; i++)
@@ -43,14 +57,13 @@ int 	main(int argc, char **argv)
 	printf("__________________\n");
 	t_node *hash_label;
 
-	for (int i = 0; i < (int)champ->labels_vec->length; i++ )
-    {
-        char *name = ((t_arg*)((t_label *)(champ->labels_vec->data[i])))->label->name;
-        printf("%s\t%c\t", name,((t_arg*)((t_label *)(champ->labels_vec->data[i])))->label->is_after);
-	    printf("%d\t", ((t_arg*)((t_label *)(champ->labels_vec->data[i])))->label->cumulate_size);
-	    if ((hash_label =  ht_find_node(champ->labels, name)))
-	        printf("%s\t%d\n", hash_label->name, hash_label->command->cumulative_size);
-    }
+	for (int i = 0; i < (int)champ->labels_vec->length; i++ ) {
+		char *name = ((t_arg *) ((t_label *) (champ->labels_vec->data[i])))->label->name;
+		printf("%s\t%c\t", name, ((t_arg *) ((t_label *) (champ->labels_vec->data[i])))->label->is_after);
+		printf("%d\t", ((t_arg *) ((t_label *) (champ->labels_vec->data[i])))->label->cumulate_size);
+		if ((hash_label = ht_find_node(champ->labels, name)))
+			printf("%s\t%d\n", hash_label->name, hash_label->command->cumulative_size);
+	}
 	return (0);
 
 
