e65b76582a57227d814dc9c899d0e029bf98e4d7
Nikolai <professor3222@gmail.com>
2020-05-18 22:43:55 +0300

    adding line on which error occured

--- src/error_manager.c
+++ src/error_manager.c
@@ -4,9 +4,9 @@
 
 #include "../includes/assembler.h"
 
-void	error_message(const char *message)
+void	error_message(int line_num, const char *message)
 {
-	ft_printf("{RED}%s{GREY}\n", message);
+	ft_printf("{RED}%s\nline num: %d{GREY}\n", message, line_num);
 }
 
 void	help(void)
@@ -18,32 +18,32 @@ void	help(void)
 void	error_manager(enum e_error_type error_type, t_champ **champ)
 {
 	if (error_type == NO_FILE)
-		error_message("No file to compile");
+		error_message((*champ)->counter, "No file to compile");
 	else if (error_type == MALLOC_ERROR)
-		error_message("Malloc error");
+		error_message((*champ)->counter, "Malloc error");
 	else if (error_type == UNKNOWN_TOKEN)
-		error_message("Unknown token to parse");
+		error_message((*champ)->counter, "Unknown token to parse");
 	else if (error_type == NO_CHAMP_NAME)
-		error_message("No champ's name");
+		error_message((*champ)->counter, "No champ's name");
 	else if (error_type == NO_CHAMP_COMMENT)
-		error_message("No champ's comment");
+		error_message((*champ)->counter, "No champ's comment");
 	else if (error_type == WRONG_LENGTH_OF_NAME)
-		error_message("Wrong length of champ's name or champs' comment");
+		error_message((*champ)->counter, "Wrong length of champ's name or champs' comment");
 	else if (error_type == INCORRECT_SYNTAX)
-		error_message("Incorrect syntax");
+		error_message((*champ)->counter, "Incorrect syntax");
 	else if (error_type == NON_EXISTING_COMMAND)
-		error_message("Non existing command");
+		error_message((*champ)->counter, "Non existing command");
 	else if (error_type == NON_EXISTING_LABEL)
-		error_message("Labels with out command");
+		error_message((*champ)->counter, "Labels with out command");
 	else if (error_type == WRONG_NUMBER_OF_ARGS)
-		error_message("Wrong number of args");
+		error_message((*champ)->counter, "Wrong number of args");
 	else if (error_type == WRONG_TYPE_OF_ARGS)
-        error_message("Wrong type of command arguments");
+        error_message((*champ)->counter, "Wrong type of command arguments");
 	else if (error_type == LABELS_WITHOUT_COMMAND)
-        error_message("Wrong label");
+        error_message((*champ)->counter, "Wrong label");
 	else if (error_type == NO_BACKSLASH)
-		error_message("No backslash at the end of the file");
+		error_message((*champ)->counter, "No backslash at the end of the file");
 	else if (error_type == UNEXPECTED_END_OF_FILE)
-		error_message("Unexpected end of file");
+		error_message((*champ)->counter, "Unexpected end of file");
 	finish_him(champ);
 }
