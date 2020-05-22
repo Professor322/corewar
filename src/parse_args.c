bff94029ca160afbb314abcd98cf21caabaf593d
Nikolai <professor3222@gmail.com>
2020-05-22 19:20:10 +0300

    parse_args norme

--- src/parse_args.c
+++ src/parse_args.c
@@ -1,10 +1,18 @@
-//
-// Created by professor on 15.03.20.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: professor <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/07 18:28:37 by vlegros           #+#    #+#             */
+/*   Updated: 2020/05/22 19:18:06 by professor        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../includes/assembler.h"
 
-int 	check_starter_token(const char starter)
+int		check_starter_token(const char starter)
 {
 	if (starter == DIRECT_CHAR)
 		return (1);
@@ -17,28 +25,28 @@ int 	check_starter_token(const char starter)
 	return (0);
 }
 
-char 	*create_arg(char **line, const char *arg_start, char *arg_end)
+char	*create_arg(char **line, const char *arg_start, char *arg_end)
 {
-	char *arg;
-	const char chr = *arg_end;
+	const char	chr = *arg_end;
+	char		*arg;
 
 	*arg_end = END_LINE;
 	arg = ft_strdup(arg_start);
 	*arg_end = chr;
 	*line = *arg_end && *arg_end != COMMENT_CHAR ? arg_end : NULL;
-	return arg;
+	return (arg);
 }
 
-char 	*check_arg_content(char **line)
+char	*check_arg_content(char **line)
 {
-	const char *arg_start = *line;
-	char *arg_end;
+	const char	*arg_start = *line;
+	char		*arg_end;
 
 	arg_end = *line;
 	if (*arg_end == REGISTER_CHAR || *arg_end == DIRECT_CHAR)
 		arg_end = (char*)arg_start + 1;
 	if (!*arg_end)
-		return NULL;
+		return (NULL);
 	if (*arg_end == LABEL_CHAR)
 	{
 		arg_end++;
@@ -52,42 +60,41 @@ char 	*check_arg_content(char **line)
 			*arg_end != SEPARATOR_CHAR &&
 			*arg_end != COMMENT_CHAR &&
 			*arg_end != ALT_COMMENT_CHAR)
-		return NULL;
-
+		return (NULL);
 	return (create_arg(line, arg_start, arg_end));
 }
 
-char 	*parse_arg(char **line, const int arg_num)
+char	*parse_arg(char **line, const int arg_num)
 {
-	char *arg;
+	char	*arg;
 
 	if (!*line)
-		return NULL;
+		return (NULL);
 	if (arg_num >= 1 && **line != SEPARATOR_CHAR)
-		return NULL;
+		return (NULL);
 	if (arg_num >= 1)
 		(*line)++;
 	skip_spaces(line);
 	if (*line && !check_starter_token(**line))
-		return NULL;
+		return (NULL);
 	if (!(arg = check_arg_content(line)))
-		return NULL;
+		return (NULL);
 	skip_spaces(line);
-	return arg;
+	return (arg);
 }
 
-char 	**parse_args(t_champ **champ_ptr, char *line, const int cmd)
+char	**parse_args(t_champ **champ_ptr, char *line, const int cmd)
 {
-	int i;
-	const int len = g_commands[cmd].num_of_args;
-	char **args;
-	char *temp;
+	int			i;
+	const int	len = g_commands[cmd].num_of_args;
+	char		**args;
+	char		*temp;
 
 	line = line + g_commands[cmd].name_len;
-	if(!(args = (char**)ft_memalloc(sizeof(char*) * (len + 1))))
+	if (!(args = (char**)ft_memalloc(sizeof(char*) * (len + 1))))
 		error_manager(MALLOC_ERROR, champ_ptr);
-	i = 0;
-	while((temp = parse_arg(&line, i)))
+	i = -1;
+	while ((temp = parse_arg(&line, ++i)))
 	{
 		if (i == g_commands[cmd].num_of_args)
 		{
@@ -96,13 +103,11 @@ char 	**parse_args(t_champ **champ_ptr, char *line, const int cmd)
 			error_manager(WRONG_NUMBER_OF_ARGS, champ_ptr);
 		}
 		args[i] = temp;
-		i++;
 	}
 	if (i < len)
 	{
 		ft_del_twodem_arr((void***)&args);
 		error_manager(WRONG_NUMBER_OF_ARGS, champ_ptr);
 	}
-	args[i] = NULL;
-	return args;
+	return (args);
 }
