12a4840134b2ecb4eea54b6633e6414d0fa53630
Nikolai <professor3222@gmail.com>
2020-06-01 16:10:17 +0300

    adding badass champion

---
+++ champion/champ.s
@@ -0,0 +1,76 @@
+.name "best champ"
+.comment "worst comment"
+
+ld %0, r2
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+st r1, 322
+loop:
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+zjmp %:loop
+
